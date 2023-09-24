89f7d452406f19e7c9935ca231ec7d6db9022b53
gamename <tennis.n.smith@gmail.com>
2023-09-24 18:43:44 -0500

    several changes 1. move models to single subdir 2. add utils.py routines to doorbell.py

--- raspberry-pi/doorbell.py
+++ raspberry-pi/doorbell.py
@@ -7,12 +7,11 @@ import RPi.GPIO as GPIO
 import board
 import cv2
 import neopixel
+import numpy as np
 import requests
-from tflite_support.task import audio, core, processor
-from tflite_support.task import vision
+from tflite_support.task import audio, core, processor, vision
 
 import my_secrets
-import utils
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -28,6 +27,51 @@ LED_STRIP_OUTPUT_PIN = board.D18  # Physical pin 12
 # Pixel color values
 ON = (255, 255, 255)  # White
 OFF = (0, 0, 0)
+
+_MARGIN = 10  # pixels
+_ROW_SIZE = 10  # pixels
+_FONT_SIZE = 1
+_FONT_THICKNESS = 1
+_TEXT_COLOR = (0, 0, 255)  # red
+
+
+def get_category_name(detection_result: processor.DetectionResult) -> str:
+    category_name = None
+    for detection in detection_result.detections:
+        category = detection.categories[0]
+        category_name = category.category_name
+    return category_name
+
+
+def visualize(image: np.ndarray, detection_result: processor.DetectionResult) -> np.ndarray:
+    """
+    Draws bounding boxes on the input image and return it.
+
+  Args:
+    image: The input RGB image.
+    detection_result: The list of all "Detection" entities to be visualized.
+
+  Returns:
+    Image with bounding boxes.
+  """
+    for detection in detection_result.detections:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
+
+        # Draw label and score
+        category = detection.categories[0]
+        category_name = category.category_name
+        probability = round(category.score, 2)
+        result_text = category_name + ' (' + str(probability) + ')'
+        text_location = (_MARGIN + bbox.origin_x,
+                         _MARGIN + _ROW_SIZE + bbox.origin_y)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+
+    return image
 
 
 def cat_image_detected(model, timeout=30) -> bool:
@@ -81,8 +125,10 @@ def cat_image_detected(model, timeout=30) -> bool:
         # Run object detection estimation using the model.
         detection_result = detector.detect(input_tensor)
 
-        print(f'{utils.get_category_name(detection_result)}')
-        if utils.get_category_name(detection_result) == 'cat':
+        category = get_category_name(detection_result)
+
+        print(f'{category}')
+        if category == 'cat':
             found = True
             break
 
@@ -93,7 +139,7 @@ def cat_image_detected(model, timeout=30) -> bool:
             break
 
         # Draw keypoints and edges on input image
-        image = utils.visualize(image, detection_result)
+        image = visualize(image, detection_result)
 
         # Calculate the FPS
         if counter % fps_avg_frame_count == 0:
@@ -117,8 +163,8 @@ def cat_image_detected(model, timeout=30) -> bool:
 
 def doorbell(args) -> None:
     # Tensorflow setup
-    model = CURRENT_DIR + '/' + str(args.model)
-    video_model = CURRENT_DIR + '/' + str(args.videoModel)
+    model = CURRENT_DIR + '/models/' + str(args.model)
+    video_model = CURRENT_DIR + '/models/' + str(args.videoModel)
     detection_pause = int(args.pauseAfterDetection)
     max_results = int(args.maxResults)
     score_threshold = float(args.scoreThreshold)
