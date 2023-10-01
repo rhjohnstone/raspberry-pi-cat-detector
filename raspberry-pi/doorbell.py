de1733daae4c325d46cbf4426f10e406c53ac92d
gamename <tennis.n.smith@gmail.com>
2023-10-01 05:50:40 -0500

    take out log msg formatting

--- raspberry-pi/doorbell.py
+++ raspberry-pi/doorbell.py
@@ -221,12 +221,8 @@ def doorbell(target_object, args):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    # this is just to make the output look nice
-    formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
-
     # this logs to stdout and I think it is flushed immediately
     handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     max_results = 1
