45bcf558eb771237beec0a22005fda573b121840
Ross Johnstone <rhjohnstone@users.noreply.github.com>
2024-01-08 19:35:51 +0900

    remove the video stuff

--- raspberry-pi/doorbell.py
+++ raspberry-pi/doorbell.py
@@ -31,7 +31,8 @@ from tflite_support.task import audio, core, processor, vision
 import my_secrets
 from lights import DoorBellLightsController
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+TARGET = "cat"
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/models/yamnet.tflite"
 
 # What GPIO pin is associated with a condition?
 DARK_INDICATOR_PIN = 2  # Physical pin 3
@@ -48,154 +49,7 @@ handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
 
 
-def get_category_name(detection_result: processor.DetectionResult) -> str:
-    """
-    Pull a specific category name from a detection result
-
-    Args:
-        detection_result: an object identification result
-
-    Returns:
-        the category name in a single string
-    """
-    category_name = None
-    for detection in detection_result.detections:
-        category = detection.categories[0]
-        category_name = category.category_name
-    return category_name
-
-
-def visualize(image: np.ndarray, detection_result: processor.DetectionResult) -> np.ndarray:
-    """
-    Draws bounding boxes on the input image and return it.
-
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualized.
-
-  Returns:
-    Image with bounding boxes.
-  """
-    _MARGIN = 10  # pixels
-    _ROW_SIZE = 10  # pixels
-    _FONT_SIZE = 1
-    _FONT_THICKNESS = 1
-    _TEXT_COLOR = (0, 0, 255)  # red
-
-    for detection in detection_result.detections:
-        # Draw bounding_box
-        bbox = detection.bounding_box
-        start_point = bbox.origin_x, bbox.origin_y
-        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-        cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
-
-        # Draw label and score
-        category = detection.categories[0]
-        category_name = category.category_name
-        probability = round(category.score, 2)
-        result_text = category_name + ' (' + str(probability) + ')'
-        text_location = (_MARGIN + bbox.origin_x,
-                         _MARGIN + _ROW_SIZE + bbox.origin_y)
-        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                    _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
-
-    return image
-
-
-def look_for(target_object, model, timeout=45) -> bool:
-    """
-    Look for a target object
-
-    Args:
-        target_object (): The thing we want to see and identify
-        model (): The model to use for identification
-        timeout (): The amount of time before we give up
-
-    Returns:
-        True if found, False if not
-    """
-    timeout_start = time.time()
-    camera_id = 0
-    width = 640
-    height = 480
-    num_threads = 4
-
-    found = False
-
-    # Start capturing video input from the camera
-    cap = cv2.VideoCapture(camera_id)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    # Initialize the object detection model
-    base_options = core.BaseOptions(file_name=model, use_coral=False, num_threads=num_threads)
-    detection_options = processor.DetectionOptions(max_results=1, score_threshold=0.3)
-    options = vision.ObjectDetectorOptions(base_options=base_options, detection_options=detection_options)
-    detector = vision.ObjectDetector.create_from_options(options)
-
-    # Continuously capture images from the camera and run inference
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            sys.exit(
-                'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-            )
-
-        image = cv2.flip(image, 1)
-
-        # Convert the image from BGR to RGB as required by the TFLite model.
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        # Create a TensorImage object from the RGB image.
-        input_tensor = vision.TensorImage.create_from_array(rgb_image)
-
-        # Run object detection estimation using the model.
-        detection_result = detector.detect(input_tensor)
-
-        # Draw keypoints and edges on input image
-        image = visualize(image, detection_result)
-
-        cv2.imshow('object_detector', image)
-
-        category = get_category_name(detection_result)
-
-        logger.info(f'Detected category: {category}')
-
-        if category == target_object:
-            logger.info(f"Category {category} == Target {target_object}.")
-            found = True
-            break
-
-        time_hack = int(time.time() - timeout_start)
-
-        if time_hack > timeout:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")[:-3]
-            outfile = f"/tmp/{timestamp}.jpg"
-            cv2.imwrite(outfile, image)
-            logger.info("Timeout period reached. Diagnostic image saved")
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return found
-
-
-def doorbell(target_object, args):
-    """
-    Listen for a target object and then look for it
-
-    Args:
-        target_object ():  The thing we want to identify
-        args (): The model info for audio and video sensing
-
-    Returns:
-        Nothing
-    """
-
-    audio_model = CURRENT_DIR + '/models/' + str(args.audioModel)
-    video_model = CURRENT_DIR + '/models/' + str(args.videoModel)
-    detection_pause = int(args.pauseAfterDetection)
+def main():
 
     max_results = 1
     score_threshold = 0.0
@@ -203,7 +57,7 @@ def doorbell(target_object, args):
     num_threads = 4
     enable_edgetpu = False
 
-    base_options = core.BaseOptions(file_name=audio_model, use_coral=enable_edgetpu, num_threads=num_threads)
+    base_options = core.BaseOptions(file_name=MODEL_PATH, use_coral=enable_edgetpu, num_threads=num_threads)
     classification_options = processor.ClassificationOptions(max_results=max_results, score_threshold=score_threshold)
     options = audio.AudioClassifierOptions(base_options=base_options, classification_options=classification_options)
 
@@ -240,46 +94,8 @@ def doorbell(target_object, args):
 
         # print(f"noise: {noise}")
 
-        if noise == target_object:
-            logger.info("Heard %s", noise)
-
-            # If it is dark, turn LEDs on so the camera can 'see' the cat
-            lights.turn_on()
-            #
-            # Now that we heard the cat, can we see him?
-            if look_for('cat', video_model):
-                logger.info("Cat heard and seen. Trigger text msg")
-                requests.post(my_secrets.REST_API_URL, headers=REQUEST_HEADER)
-
-                logger.info(f"Pausing for {detection_pause} seconds")
-                time.sleep(detection_pause)
-                logger.info("Pause over. Continuing")
-            else:
-                logger.info("Could not see cat.")
-
-            lights.turn_off()
-
-
-def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('--videoModel',
-                        help='Video classification model file.',
-                        required=False,
-                        default='efficientdet_lite0.tflite')
-    #
-    parser.add_argument('--audioModel',
-                        help='Audio classification model file.',
-                        required=False,
-                        default='yamnet.tflite')
-    #
-    parser.add_argument('--pauseAfterDetection',
-                        help='Number of seconds to pause after a positive result.',
-                        required=False,
-                        default=120)
-    args = parser.parse_args()
-
-    doorbell('cat', args)
+        if noise == TARGET:
+            logger.info(f"Heard {noise}")
 
 
 if __name__ == '__main__':
